c3f1d5d0e2927ea852408f5c37e9b81ddea55379
ninanyu <ninanyu0704@gmail.com>
2024-10-07 13:04:04 +0900

    4주차 스터디 내역

---
+++ 4week/4.Quiz(2).js
@@ -0,0 +1,22 @@
+/*1. 다음 헤더코드를 컨포넌트로 만드시오.
+<!DOCTYPE html>
+<html>
+  <body>
+    <!-- HEADER -->
+    <header>
+      <h1>안녕하세요 이정환입니다</h1>
+    </header>
+    <article>
+      <!-- 페이지별로 필요한 요소를 작성합니다 -->
+    </article>
+  </body>
+</html> */
+
+function MyHeader() {
+  return (
+    <header>
+      <h1>안녕하세요 수정된 이정환입니다.</h1>
+    </header>
+  );
+}
+
